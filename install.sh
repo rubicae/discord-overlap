372460679cf37b84898d6b1199ed1a507c77941d
rubicae <rubicae@[::1]>
2019-04-01 02:29:28 -0700

    Mac OS X sed differences

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ fi
 source venv/bin/activate
 pip install -r requirements.txt
 grep -l -R 'asyncio.async' venv/lib/python*/site-packages/ |\
- xargs sed -i 's/asyncio.async/asyncio.__dict__["async"]/g'
+ xargs sed -ibak 's/asyncio.async/asyncio.__dict__["async"]/g'
